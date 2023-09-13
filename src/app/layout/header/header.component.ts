7e931d9e302bf4f46e0f4e7e743f7e3462f84913
Muhammad Awais <awaisshaikh94@gmail.com>
2023-09-13 19:45:04 +0500

    sidebar done with shareable scss files

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common'
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  toggleButton: boolean = false;
+  sidebar: boolean = false;
+  fadeSection: boolean = false;
+  backBtnState: boolean = false;
+
+  routes: any = [
+    { name: "Home", path: '/'},
+    { name: "About", path: '/'},
+    { name: "Finances", path: '/'},
+    { name: "Insurance", path: '/'},
+  ]
+
+  constructor(
+    private location: Location,
+  ) {}
+
+  toggleMenu() {
+    // animating icon
+    this.toggleButton = !this.toggleButton;
+
+    // animating sidebar
+    this.sidebar = !this.sidebar;
+
+    // enabling fade section 
+    this.fadeSection = !this.fadeSection;
+  }
+
+  back() {
+    this.location.back();
+  }
+
+}
