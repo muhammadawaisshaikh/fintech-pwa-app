e70442ea23239e07a51666035efd93ed2a6a8060
Muhammad Awais <awaisshaikh94@gmail.com>
2023-09-14 13:48:51 +0500

    alert of larger screen visit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'fintech-angular';
+
+  ngOnInit(): void {
+    if (window.innerWidth > 768) {
+      alert('Mobile Only! UI will be compromised becuase you are visitng it on larger screens');
+    }
+  }
 }
