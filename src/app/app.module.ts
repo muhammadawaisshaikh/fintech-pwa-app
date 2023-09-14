64f179c0eb55d0656392d47ca12cfea05cdf6534
Muhammad Awais <awaisshaikh94@gmail.com>
2023-09-14 13:30:31 +0500

    bottom bar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { BottomBarComponent } from './layout/bottom-bar/bottom-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BottomBarComponent
   ],
   imports: [
     BrowserModule,
